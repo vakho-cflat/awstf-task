8be74a59291f9c7fd20e2c28367a1547ab94c698
Vakho <vaxotabatadzee@gmail.com>
2023-12-30 01:28:51 +0400

    adding state lock timeout

--- backend.tf
+++ backend.tf
@@ -4,5 +4,9 @@ terraform {
     key            = "./terraform.tfstate"
     region         = "eu-central-1"
     dynamodb_table = ""
+   
+     config {
+      max_lock_duration_seconds = 150
+    }
   }
 }
